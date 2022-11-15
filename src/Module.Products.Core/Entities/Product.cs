f5dec000c9ab1860f3a418111a22651e49deb635
Leonardo Ferreira <leonardo.limaferreira@yahoo.com.br>
2022-11-15 10:29:02 -0300

    Add products controller and entitity

---
+++ src/Module.Products.Core/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Module.Products.Core.Entities
+{
+    internal class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/Module.Products/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Module.Products.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    internal class ProductsController : ControllerBase
+    {
+        [HttpGet] 
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete()
+        {
+            return Ok();
+        }
+    }
+}
