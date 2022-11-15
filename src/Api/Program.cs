28876ed8a99d5ddbb75360840e9d107a3c3f9873
Leonardo Ferreira <leonardo.limaferreira@yahoo.com.br>
2022-11-15 01:02:50 -0300

    add internal controllers configuration

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,8 +1,10 @@
+using Shared.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddSharedInfrastructure(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
